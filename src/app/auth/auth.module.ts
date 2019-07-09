11f77152362baee29fb7c8168f3a4e77473888d7
GustavoSilvaFerreira <guto7626@hotmail.com>
2019-07-09 17:10:31 -0300

    módulo de autenticação

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+];
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class AuthModule { }
