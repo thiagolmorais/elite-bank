e37b8353a6bf3fdfb646c8517895abee3dbc12eb
Luiz Ramos <luizgdramos@gmail.com>
2019-07-10 14:57:00 -0300

    Alterando padrão scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,50 +4,50 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
-  user$: Observable<any>;
+    user$: Observable<any>;
 
-  constructor(private authService: AuthService,
-              private router: Router) {}
+    constructor(private authService: AuthService,
+        private router: Router) { }
 
-  ngOnInit() {
-    const token = localStorage.getItem('token');
+    ngOnInit() {
+        const token = localStorage.getItem('token');
 
-    if(!token) {
-      this.router.navigateByUrl('/auth');
-    } else {
-      //  VERIFICAR TOKEN AQUI COM SUBSCRIBE
-      this.authService.checkToken(token)
-      .then((value: any) => {
-  
-        this.authService.setUser({
-          name: value.name,
-          email: value.email,
-          account: value.account
-        });
-      })
-      .catch((error) => {
-        alert(error);
-        console.log(error);
-      });
-      // .subscribe((v: any) => {
-      //   this.authService.setUser({
-      //     id: v.users[0].localId,
-      //     email: v.users[0].email,
-      //   });
-      // });
+        if (!token) {
+            this.router.navigateByUrl('/auth');
+        } else {
+            //  VERIFICAR TOKEN AQUI COM SUBSCRIBE
+            this.authService.checkToken(token)
+                .then((value: any) => {
+
+                    this.authService.setUser({
+                        name: value.name,
+                        email: value.email,
+                        account: value.account
+                    });
+                })
+                .catch((error) => {
+                    alert(error);
+                    console.log(error);
+                });
+            // .subscribe((v: any) => {
+            //   this.authService.setUser({
+            //     id: v.users[0].localId,
+            //     email: v.users[0].email,
+            //   });
+            // });
+        }
+
+        this.user$ = this.authService.correntUser;
     }
 
-    this.user$ = this.authService.correntUser;
-  }
+    logout() {
+        this.authService.logout();
+    }
 
-  logout() {
-    this.authService.logout();
-  }
-  
 }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,66 +3,66 @@ import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private authService: AuthService,
-              private router: Router) { }
+    constructor(private authService: AuthService,
+        private router: Router) { }
 
-  LoginData = {
-    account: '98765',
-    password: '123456'
-  }
+    LoginData = {
+        account: '98765',
+        password: '123456'
+    }
 
-  ngOnInit() {
-    this.login();
-  }
+    ngOnInit() {
+        this.login();
+    }
 
-  login() {
-    this.authService.login(this.LoginData.account, this.LoginData.password)
-    // .subscribe((value: any) => {
-    //   alert('Login efetuado com sucesso!!!');
-    //   localStorage.setItem('token', value.idToken);
+    login() {
+        this.authService.login(this.LoginData.account, this.LoginData.password)
+            // .subscribe((value: any) => {
+            //   alert('Login efetuado com sucesso!!!');
+            //   localStorage.setItem('token', value.idToken);
 
-    //   this.authService.setUser({
-    //     id: value.localId,
-    //     email: value.email,
-    //   });
+            //   this.authService.setUser({
+            //     id: value.localId,
+            //     email: value.email,
+            //   });
 
-    //   this.router.navigateByUrl('/');
-    // },
-    // (error) => {
-    //   switch (error.error.error.message) {
-    //     case 'EMAIL_NOT_FOUND':
-    //       alert('E-mail não encontrado');
-    //       break;
-    //     case 'INVALID_PASSWORD':
-    //       alert('Senha inválida');
-    //       break;
-    //     default:
-    //       alert('Houve um erro');
-    //       break;
-    //   }
-    // });
-    .then((value: any) => {
-      alert('Login efetuado com sucesso!!!');
-      localStorage.setItem('token', value.token);
+            //   this.router.navigateByUrl('/');
+            // },
+            // (error) => {
+            //   switch (error.error.error.message) {
+            //     case 'EMAIL_NOT_FOUND':
+            //       alert('E-mail não encontrado');
+            //       break;
+            //     case 'INVALID_PASSWORD':
+            //       alert('Senha inválida');
+            //       break;
+            //     default:
+            //       alert('Houve um erro');
+            //       break;
+            //   }
+            // });
+            .then((value: any) => {
+                alert('Login efetuado com sucesso!!!');
+                localStorage.setItem('token', value.token);
 
-      this.authService.setUser({
-        name: value.name,
-        email: value.email,
-        account: value.account
-      });
+                this.authService.setUser({
+                    name: value.name,
+                    email: value.email,
+                    account: value.account
+                });
 
-      this.router.navigateByUrl('/');
-    })
-    .catch((error) => {
-      alert(error);
-      console.log(error);
-    });
-  }
+                this.router.navigateByUrl('/');
+            })
+            .catch((error) => {
+                alert(error);
+                console.log(error);
+            });
+    }
 
 }
