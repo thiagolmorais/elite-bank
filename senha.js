f8bf78a1c7e9667f80a76e9c96425dde39343a67
Thiago Morais <tf_lima@terra.com.br>
2019-07-11 15:13:12 -0300

    Delete senha.js

--- senha.js
+++
@@ -1,8 +0,0 @@
-const senhaMongo = 12345
-const senhaFront = [ {firstNumber: 4, secondNumber: 1}, {firstNumber: 2, secondNumber: 7}, {firstNumber: 5, secondNumber: 3}, {firstNumber: 4, secondNumber:1}, {firstNumber: 5, secondNumber:3} ]
-
-function autenticar(senhaFront) {
-    senhaMongo.even(
-        
-    )
-}
