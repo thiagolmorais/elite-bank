ff9923bcb38910f5e61b89000108fee47c1fbe50
Luiz Ramos <luizgdramos@gmail.com>
2019-07-12 13:44:52 -0300

    Corrigindo componente de chamada

--- src/app/logged/logged.module.ts
+++ src/app/logged/logged.module.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', component: LoggedComponent },
 ];
 
 @NgModule({
