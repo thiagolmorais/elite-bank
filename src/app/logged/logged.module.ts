dd457c5f76028caf4c74cd4c578ad7a42f36dcc5
Thiago Morais <tf_lima@terra.com.br>
2019-07-13 04:13:54 -0300

    Update logged.module.ts

--- src/app/logged/logged.module.ts
+++ src/app/logged/logged.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
     declarations: [
         LoggedComponent,
         HomeComponent,
-        TransferComponent
+        TransferComponent,
         ExtractComponent
     ],
     imports: [
