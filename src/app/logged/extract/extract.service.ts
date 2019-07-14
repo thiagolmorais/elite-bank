6de2ea974ec0d318c6c120f7ff1e8e376ed99511
Thiago Morais <tf_lima@terra.com.br>
2019-07-14 04:31:20 -0300

    Add extract

--- src/app/logged/extract/extract.service.ts
+++ src/app/logged/extract/extract.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
 import { ELITE_BANK_API } from 'src/app/app.api';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExtractService {
 
-  constructor(private authService: AuthService) { }
+  constructor(private httpClient: HttpClient) { }
 
-  getExtracts() {
-
+  getExtracts(accountNumber) {
+    return this.httpClient.get(`${ELITE_BANK_API}/accounts/${accountNumber}/transfers`);
   }
 }
