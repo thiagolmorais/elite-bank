cce07c9392fb8492a305f28816643d7cf991f3dc
Luiz Ramos <luizgdramos@gmail.com>
2019-07-14 13:46:52 -0300

    Separando o form de login em um componente

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,75 +4,76 @@ import { BehaviorSubject } from 'rxjs';
 
 import { ELITE_BANK_API } from '../app.api';
 import { Router } from '@angular/router';
+import { Login } from 'src/model/login';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-  private pUser = new BehaviorSubject(null);
-  currentUser = this.pUser.asObservable();
+    private pUser = new BehaviorSubject(null);
+    currentUser = this.pUser.asObservable();
 
-  constructor(private httpClient: HttpClient,
-              private router: Router) { }
+    constructor(private httpClient: HttpClient,
+        private router: Router) { }
 
-  login(account, password) {
-    return this.httpClient.post(`${ELITE_BANK_API}/login`, {
-      account,
-      password
-    });
-  };
-
-  setUser(user) {
-    this.pUser.next(user);
-  };
-
-  checkToken() {
-    const token = localStorage.getItem('token');
-    const account = localStorage.getItem('account');
-
-    if (!token) {
-        this.router.navigateByUrl('/login');
-    } else {
-      return this.httpClient.post(`${ELITE_BANK_API}/checktoken`, {
-        account: account,
-        token: token
-      })
-      .subscribe((dados: any) => {
-        const { response, message } = dados;
-        if(!response) {
-            alert(`Erro: ${message}`);
-            localStorage.removeItem('token');
-            localStorage.removeItem('account');
-            this.router.navigateByUrl('/login');
-            return;
-        }
-  
-        const { account, balance, name } = message;
-  
-        this.setUser({
-            name: name,
-            balance: balance,
-            account: account
+    login(loginData: Login) {
+        return this.httpClient.post(`${ELITE_BANK_API}/login`, {
+            account: loginData.account,
+            password: loginData.password
         });
-      });
-    }
-  };
+    };
 
-  logout() {
-    const token = localStorage.getItem('token');
-    const account = localStorage.getItem('account');
+    setUser(user) {
+        this.pUser.next(user);
+    };
 
-     this.httpClient.post(`${ELITE_BANK_API}/logout`, {
-      account: account,
-      token: token
-    })
-    .subscribe(() => {
-      localStorage.removeItem('token');
-      localStorage.removeItem('account');
-      
-      this.router.navigateByUrl('/login');
-    });
-  };
+    checkToken() {
+        const token = localStorage.getItem('token');
+        const account = localStorage.getItem('account');
+
+        if (!token) {
+            this.router.navigateByUrl('/login');
+        } else {
+            return this.httpClient.post(`${ELITE_BANK_API}/checktoken`, {
+                account: account,
+                token: token
+            })
+                .subscribe((dados: any) => {
+                    const { response, message } = dados;
+                    if (!response) {
+                        alert(`Erro: ${message}`);
+                        localStorage.removeItem('token');
+                        localStorage.removeItem('account');
+                        this.router.navigateByUrl('/login');
+                        return;
+                    }
+
+                    const { account, balance, name } = message;
+
+                    this.setUser({
+                        name: name,
+                        balance: balance,
+                        account: account
+                    });
+                });
+        }
+    };
+
+    logout() {
+        const token = localStorage.getItem('token');
+        const account = localStorage.getItem('account');
+
+        this.httpClient.post(`${ELITE_BANK_API}/logout`, {
+            account: account,
+            token: token
+        })
+            .subscribe(() => {
+                localStorage.removeItem('token');
+                localStorage.removeItem('account');
+
+                this.router.navigateByUrl('/login');
+            });
+    };
 
 }
